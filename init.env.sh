208404fabe16dbb26df54ca27e54fa81d336d6c8
keita.oouchi <keita.oouchi@gmail.com>
2021-11-14 18:39:34 +0900

    java追加など

--- init.env.sh
+++ init.env.sh
@@ -8,15 +8,23 @@ if which rbenv >/dev/null 2>&1; then
   eval "$(rbenv init -)"
 fi
 
+# go
 if [ ! -d $HOME/.go ]; then
   mkdir $HOME/.go
 fi
 export GOPATH=$HOME/.go
 
+# python
 if which pyenv >/dev/null 2>&1; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init --path)"
   # poetry
-  PATH=$HOME/.poetry/bin:$PATH
+  export PATH=$HOME/.poetry/bin:$PATH
+fi
+
+# java & android
+export JAVA_HOME=`/usr/libexec/java_home`
+if [ -d $HOME/Library/Android/sdk/platform-tools ]; then
+  export PATH=$HOME/Library/Android/sdk/platform-tools:$HOME/Library/Android/sdk/tools:$PATH
 fi
--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ ln -s $HOME/dotfiles_public/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles_public/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles_public/gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles_public/gitignore_global $HOME/.gitignore_global
+ln -s $HOME/dotfiles_public/tmux.conf $HOME/.tmux.conf
 
 brew install \
     direnv git jq openssl pyenv rbenv nodenv \
     zsh-completions zsh-syntax-highlighting \
-    poetry
+    poetry tmux
